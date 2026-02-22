320aec2f8a7710e3dc2eccc05b8073b8fa2b96a4
Kristina Fefelova <kristin.fefelova@gmail.com>
2026-02-22 18:28:19 +0400

    Remove unused sql migration
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- foundations/server/packages/postgres/migrations/chunterSchema.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE chunter_doc
-    ADD COLUMN IF NOT EXISTS "attachedToClass" text,
-    ADD COLUMN IF NOT EXISTS "account" text,
-    ADD COLUMN IF NOT EXISTS "pinned" boolean DEFAULT false,
-    ADD COLUMN IF NOT EXISTS "hidden" boolean DEFAULT false;
