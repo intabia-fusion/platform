273dff5c0f71347d15e144b94ae3f8b6ab756f7c
Andrey Sobolev <haiodo@gmail.com>
2026-03-05 16:50:19 +0700

    Improve stats service + uptrace
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- services/github/pod-github/run.sh
+++ services/github/pod-github/run.sh
@@ -6,6 +6,6 @@ export SERVER_SECRET=secret
 export ACCOUNTS_URL=http://localhost:3000
 export COLLABORATOR_URL=ws://huly.local:3078
 export STORAGE_CONFIG="datalake|http://huly.local:4030"
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://huly.local:4318/v1/traces
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://huly.local:4318
 rush bundle --to @hcengineering/pod-github
 node $@ bundle/bundle.js $@
