2c7acd2ce14e9e3cbe17c21fea57d3444e990898
Kristina Fefelova <kristin.fefelova@gmail.com>
2026-03-19 22:35:17 +0400

    Fix copyright
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- plugins/chunter-resources/src/components/DeleteMessageConfirmationPopup.svelte
+++ plugins/chunter-resources/src/components/DeleteMessageConfirmationPopup.svelte
@@ -1,6 +1,5 @@
 <!--
-// Copyright © 2020 Anticrm Platform Contributors.
-//
+// Copyright © 2026 Intabia Fusion.
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
 // obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
