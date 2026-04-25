0e84c2043b01a719043ef625fade98d69568f980
Andrey Sobolev <haiodo@gmail.com>
2026-04-25 22:12:54 +0700

    FUSIO-343: Fix kanban show more layout
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- packages/kanban/src/components/KanbanRow.svelte
+++ packages/kanban/src/components/KanbanRow.svelte
@@ -159,8 +159,8 @@
 {/each}
 {#if stateObjects.length > limitedObjects.size + (isDragging ? 1 : 0)}
   <div class="p-1 flex-no-shrink clear-mins">
-    <div class="card-container flex-between p-4">
-      <span class="caption-color">{limitedObjects.size}</span> / {stateObjects.length}
+    <div class="card-container flex-center flex-row-center p-4 gap-2 no-word-wrap">
+      <span class="caption-color">{limitedObjects.size}</span> <span>/</span><span>{stateObjects.length}</span>
       <Button
         size={'small'}
         icon={IconMoreH}
