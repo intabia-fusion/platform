18f2ba265fc566307af5115b3dd692fa5c60c17b
Andrey Sobolev <haiodo@users.noreply.github.com>
2026-05-11 23:42:05 +0700

    Huly fixes apr24 (#159)
    * Add min and max transformation functions (#10782)
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    
    * version_creating_fix (#10785)
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    
    * run_process_backend (#10791)
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    
    * Handle mixin for todo result (#10792)
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    
    * feat: add trigger to remove associated execution data when a card is deleted (#10795)
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    
    ---------
    
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>
    Co-authored-by: Denis Bykhov <bykhov.denis@gmail.com>

--- models/server-process/src/index.ts
+++ models/server-process/src/index.ts
@@ -303,6 +303,14 @@ export function createModel (builder: Builder): void {
     func: serverProcess.transform.Floor
   })
 
+  builder.mixin(process.function.Min, process.class.ProcessFunction, serverProcess.mixin.FuncImpl, {
+    func: serverProcess.transform.Min
+  })
+
+  builder.mixin(process.function.Max, process.class.ProcessFunction, serverProcess.mixin.FuncImpl, {
+    func: serverProcess.transform.Max
+  })
+
   builder.mixin(process.function.Offset, process.class.ProcessFunction, serverProcess.mixin.FuncImpl, {
     func: serverProcess.transform.Offset
   })
@@ -470,6 +478,15 @@ export function createModel (builder: Builder): void {
     trigger: serverProcess.trigger.OnCardCreate,
     txMatch: {
       _class: core.class.TxCreateDoc,
+      objectClass: cardPlugin.class.Card
+    },
+    isAsync: true
+  })
+
+  builder.createDoc(serverCore.class.Trigger, core.space.Model, {
+    trigger: serverProcess.trigger.OnCardRemove,
+    txMatch: {
+      _class: core.class.TxRemoveDoc,
       objectClass: cardPlugin.class.Card
     },
     isAsync: true
