49f554a7bf0fb456f868a87f02f8776b3656c816
Andrey Sobolev <haiodo@gmail.com>
2026-05-18 11:24:53 +0700

    Rollback retain on failure for tests
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- tests/sanity/tests/playwright.config.ts
+++ tests/sanity/tests/playwright.config.ts
@@ -35,7 +35,7 @@ const config: PlaywrightTestConfig = {
           height: 900
         },
         trace: {
-          mode: 'on',
+          mode: 'retain-on-failure',
           snapshots: true,
           screenshots: true,
           sources: true
