5e5536c9950bccd78af18da635d71ae23f0521dd
Kristina Fefelova <kristin.fefelova@gmail.com>
2026-05-21 17:56:16 +0400

    fusio-690
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- plugins/chunter-resources/src/components/ForwardMessageDialog.svelte
+++ plugins/chunter-resources/src/components/ForwardMessageDialog.svelte
@@ -48,7 +48,8 @@
       const op = client.apply('forward-message')
       const forwardData = await getForwardData(message)
       for (const _doc of selectedDocs) {
-        const employee = hierarchy.isDerived(_doc._class, contact.mixin.Employee) ? (_doc as Employee) : undefined
+        const isEmployee = hierarchy.hasMixin(_doc, contact.mixin.Employee)
+        const employee = isEmployee ? (_doc as Employee) : undefined
         const account = employee?.personUuid
         const doc = account != null ? await createAndGetDirect(client, [account, me.uuid]) : _doc
         if (doc == null) continue
