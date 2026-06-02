faa655df99e47499257ebc5edce3b672a1ea74e5
Kristina <kristin.fefelova@gmail.com>
2026-06-02 05:56:40 +0400

    Fix cross workspace notifications (#215)

--- foundations/server/packages/server/src/sessionManager.ts
+++ foundations/server/packages/server/src/sessionManager.ts
@@ -1,5 +1,6 @@
 //
 // Copyright © 2022 Hardcore Engineering Inc.
+// Copyright © 2026 Intabia Fusion.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
@@ -95,6 +96,7 @@ import {
   type WorkspaceStatistics
 } from '@hcengineering/server-core'
 import { generateToken, type Token } from '@hcengineering/server-token'
+import os from 'os'
 import { ClientSession } from './client'
 import { sendResponse } from './utils'
 import { Workspace } from './workspace'
@@ -117,6 +119,7 @@ export interface Timeouts {
 }
 
 export class TSessionManager implements SessionManager {
+  readonly transactorId: string
   private readonly statusPromises = new Map<string, Promise<void>>()
   readonly workspaces = new Map<WorkspaceUuid, Workspace>()
   checkInterval: any
@@ -159,6 +162,7 @@ export class TSessionManager implements SessionManager {
     readonly queue: PlatformQueue,
     readonly pipelineFactory: PipelineFactory
   ) {
+    this.transactorId = process.env.TRANSACTOR_ID ?? os.hostname()
     if (this.doHandleTick) {
       this.checkInterval = setInterval(() => {
         this.handleTick()
@@ -170,7 +174,7 @@ export class TSessionManager implements SessionManager {
     this.workspaceConsumer = this.queue.createConsumer<QueueWorkspaceMessage>(
       ctx.newChild('ws-queue-consume', {}, { span: false }),
       QueueTopic.Workspace,
-      generateId(),
+      this.transactorId,
       async (ctx, msg) => {
         const m = msg.value
         if (
@@ -187,7 +191,7 @@ export class TSessionManager implements SessionManager {
     this.onlineUserTxConsumer = this.queue.createConsumer<QueueOnlineUserTx>(
       ctx.newChild('transactor-online-user-tx-consumer', {}, { span: false }),
       QueueTopic.OnlineUserTx,
-      'transactor-online-user-tx',
+      this.transactorId,
       async (ctx, msg) => {
         const { workspaceUuid, tx, account } = msg.value
         const workspace = this.workspaces.get(workspaceUuid)
