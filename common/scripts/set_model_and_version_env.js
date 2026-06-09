9250568fab6985127d350cb389e470655234e841
i.ilinykh <i.ilinykh@intabia.ru>
2026-06-09 13:00:02 +0700

    FUSIO-835 - Доработка скрипта для проставления версии
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- common/scripts/set_model_and_version_env.js
+++ common/scripts/set_model_and_version_env.js
@@ -1,7 +1,11 @@
 const { execSync } = require('child_process')
 
-process.env.MODEL_VERSION = execSync('node ../common/scripts/show_version.js', { encoding: 'utf8' }).trim()
-process.env.VERSION = execSync('node ../common/scripts/show_tag.js', { encoding: 'utf8' }).trim()
+function clean(value) {
+  return value.trim().replace(/^["']|["']$/g, '')
+}
+
+process.env.MODEL_VERSION = clean(execSync('node ../common/scripts/show_version.js', { encoding: 'utf8' }))
+process.env.VERSION = clean(execSync('node ../common/scripts/show_tag.js', { encoding: 'utf8' }))
 
 const command = process.argv.slice(2).join(' ')
 execSync(command, { stdio: 'inherit', shell: true })
