8250bc97c1fba4be09aa210eb817d15474fb481c
Andrey Sobolev <haiodo@gmail.com>
2026-06-09 22:07:50 +0700

    FUSIO-835: Few more improvements
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- common/scripts/set_model_and_version_env.js
+++ common/scripts/set_model_and_version_env.js
@@ -1,11 +1,26 @@
-const { execSync } = require('child_process')
+const { execFileSync, spawnSync } = require('child_process')
+const path = require('path')
 
 function clean(value) {
   return value.trim().replace(/^["']|["']$/g, '')
 }
 
-process.env.MODEL_VERSION = clean(execSync('node ../common/scripts/show_version.js', { encoding: 'utf8' }))
-process.env.VERSION = clean(execSync('node ../common/scripts/show_tag.js', { encoding: 'utf8' }))
+function run(script) {
+  return clean(execFileSync(process.execPath, [path.join(__dirname, script)], { encoding: 'utf8' }))
+}
 
-const command = process.argv.slice(2).join(' ')
-execSync(command, { stdio: 'inherit', shell: true })
+process.env.MODEL_VERSION = run('show_version.js')
+process.env.VERSION = run('show_tag.js')
+
+const [command, ...args] = process.argv.slice(2)
+if (command === undefined) {
+  console.error('set_model_and_version_env: no command provided')
+  process.exit(1)
+}
+
+// shell only on Windows for .cmd shims; elsewhere avoid arg re-parsing
+const res = spawnSync(command, args, { stdio: 'inherit', shell: process.platform === 'win32' })
+if (res.error) {
+  console.error(res.error)
+}
+process.exit(res.status ?? 1)
