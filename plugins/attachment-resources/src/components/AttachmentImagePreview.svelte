cc17ff5d0ddeaf8ce96b0ad83001caa870cd7af0
i.ilinykh <i.ilinykh@intabia.ru>
2026-07-31 16:28:19 +0700

    FUSIO-1105 - Исправление отображения картинок в чате

--- plugins/attachment-resources/src/components/AttachmentImagePreview.svelte
+++ plugins/attachment-resources/src/components/AttachmentImagePreview.svelte
@@ -87,7 +87,11 @@
   }
 </script>
 
-<div class="container" class:loading style="width:{toStyle(dimensions.width)}; height:{toStyle(dimensions.height)}">
+<div
+  class="container"
+  class:loading
+  style="max-width: {toStyle(dimensions.width)}; width: 100%; aspect-ratio: {dimensions.width} / {dimensions.height};"
+>
   {#if error}
     {#if value.metadata?.thumbnail?.blurhash !== undefined}
       <Blurhash blurhash={value.metadata.thumbnail.blurhash} />
@@ -114,7 +118,7 @@
 
 <style lang="scss">
   .container {
-    display: inline-flex;
+    display: inline-block;
     background-color: var(--theme-link-preview-bg-color);
     border-radius: 0.75rem;
     overflow: visible;
