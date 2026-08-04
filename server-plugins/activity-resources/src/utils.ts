4209599ab6f40c984a75898d4c12d0ca5e6f7bb0
Kristina Fefelova <kristin.fefelova@gmail.com>
2026-08-04 16:15:20 +0400

    formatting
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- server-plugins/activity-resources/src/utils.ts
+++ server-plugins/activity-resources/src/utils.ts
@@ -1,8 +1,4 @@
-import core, {
-  type Class,
-  type Ref,
-  type Type
-} from '@hcengineering/core'
+import core, { type Class, type Ref, type Type } from '@hcengineering/core'
 
 export function isMarkupType (type: Ref<Class<Type<any>>>): boolean {
   return type === core.class.TypeMarkup
