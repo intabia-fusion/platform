1b60799a8f6eb8be4ba856595d9101476599dd51
Kristina Fefelova <kristin.fefelova@gmail.com>
2026-08-10 16:34:38 +0400

    Hide github issues
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- models/all/src/index.ts
+++ models/all/src/index.ts
@@ -367,9 +367,7 @@ export default function buildModel (): Builder {
         description: github.string.ConfigDescription,
         enabled: false,
         beta: false,
-        icon: github.icon.Github,
-        classFilter: defaultFilter,
-        mixinFilter: defaultMixinFilter
+        icon: github.icon.Github
       }
     ],
     [
