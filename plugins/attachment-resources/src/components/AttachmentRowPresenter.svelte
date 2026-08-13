83d7b0a94f451f4aff4de53209a1cd12880ca046
i.ilinykh <i.ilinykh@intabia.ru>
2026-08-13 13:38:54 +0700

    FUSIO-940 - Правки после ревью

--- plugins/attachment-resources/src/components/AttachmentRowPresenter.svelte
+++ plugins/attachment-resources/src/components/AttachmentRowPresenter.svelte
@@ -1,12 +1,25 @@
+<!--
+// Copyright © 2026 Intabia Fusion.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
 <script lang="ts">
   import { type Attachment } from '@hcengineering/attachment'
-  import { AccountRole, getCurrentAccount, type PersonId } from '@hcengineering/core'
-  import { getAllSocialStringsByPersonRef, getCurrentEmployee } from '@hcengineering/contact'
+  import { AccountRole, getCurrentAccount } from '@hcengineering/core'
   import { getResource } from '@hcengineering/platform'
-  import { getClient, MessageBox } from '@hcengineering/presentation'
+  import { MessageBox } from '@hcengineering/presentation'
   import { showPopup } from '@hcengineering/ui'
   import view from '@hcengineering/view'
-  import { onMount } from 'svelte'
+  import { mySocialStringsStore } from '../stores'
 
   import attachmentPlugin from '../plugin'
   import AttachmentPresenter from './AttachmentPresenter.svelte'
@@ -14,14 +27,11 @@
   export let value: Attachment | undefined
 
   const me = getCurrentAccount()
-  let mySocialStrings = new Set<string>()
 
-  onMount(async () => {
-    const client = getClient()
-    mySocialStrings = new Set(await getAllSocialStringsByPersonRef(client, getCurrentEmployee()))
-  })
-
-  $: isOwn = value !== undefined && (me.role === AccountRole.Owner || mySocialStrings.has(value.createdBy as PersonId))
+  $: isOwn =
+    value?.createdBy !== undefined &&
+    $mySocialStringsStore !== undefined &&
+    (me.role === AccountRole.Owner || $mySocialStringsStore.has(value.createdBy))
 
   function removeAttachmentWithConfirmation (attachment: Attachment): void {
     showPopup(
